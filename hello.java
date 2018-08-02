a4b0075c94388f4998657cb820c76ee7d0e035c1
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2018-08-02 10:30:06 +0900

    feat: hello world

---
+++ hello.java
@@ -0,0 +1,7 @@
+// javac hello.java                                                                                                                                                                       13:30:20  ☁  master ☂ ⚡ ✭
+// java hello
+class hello {
+  public static void main(String args[]) {
+    System.out.println("Hello World");
+  }
+}
